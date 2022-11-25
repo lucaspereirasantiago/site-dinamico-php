c0c08673bdccb5d40f8e38f5f1ba60db839dae91
lucaspereirasantiago <lucaspereirasantiago@yandex.com>
2022-11-25 20:21:58 -0300

    criando pasta pages e páginas de navegação

---
+++ app/pages/page3.php
@@ -0,0 +1,15 @@
+<?php include_once("app/views/header.php"); ?>
+<div class="container">
+    <div class="row">
+        <h1>Página 3</h1>
+    </div>
+    <div class="row">
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Deserunt dolorem blanditiis atque hic incidunt expedita
+            veniam natus deleniti enim, fugit pariatur beatae autem?
+            Porro esse repellendus ad nam obcaecati totam.
+        </p>
+    </div>
+</div>
+<?php include_once("app/views/footer.php"); ?>
