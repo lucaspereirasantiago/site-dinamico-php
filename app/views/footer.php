6e9fc8daff73b4ae68d47b894fe77f39ccccde10
lucaspereirasantiago <lucaspereirasantiago@yandex.com>
2022-11-25 20:17:41 -0300

    criando pasta views e dividindo index.php

---
+++ app/views/footer.php
@@ -0,0 +1,9 @@
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <p id="copy">&copy Copyright - Meu Site</p>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ app/views/home.php
@@ -0,0 +1,15 @@
+<main>
+    <div class="container">
+        <div class="row">
+            <h1>Bem Vindo!</h1>
+        </div>
+        <div class="row">
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Laudantium eligendi id incidunt error dolores, adipisci atque
+                molestiae cum harum vero tenetur ratione, nobis labore illo quas
+                quaerat aperiam similique perferendis!
+            </p>
+        </div>
+    </div>
+</main>
