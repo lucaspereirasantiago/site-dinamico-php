f53437e71728fc56e9b5dda3d385cdd357f0d384
lucaspereirasantiago <lucaspereirasantiago@yandex.com>
2022-11-25 20:13:03 -0300

    init

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Site Dinâmico PHP</title>
+
+        <link rel="stylesheet" href="app/css/styles.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="container">
+                    <div class="row">
+                        <span id="logo">&#128578</span>
+                    </div>
+                    <div class="row">
+                        <h1 id="title">MEU SITE</h1>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row" id="navigation">
+                        <a href="?id=home">Home</a>
+                        <a href="?id=page1">Página 1</a>
+                        <a href="?id=page2">Página 2</a>
+                        <a href="?id=page3">Página 3</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row">
+                    <h1>Bem Vindo!</h1>
+                </div>
+                <div class="row">
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        Laudantium eligendi id incidunt error dolores, adipisci atque
+                        molestiae cum harum vero tenetur ratione, nobis labore illo quas
+                        quaerat aperiam similique perferendis!
+                    </p>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <p id="copy">&copy Copyright - Meu Site</p>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
